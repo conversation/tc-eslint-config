c1046c94d5f55b07067c184993b99b876a467834
nickbrowne <nickbrowne@users.noreply.github.com>
2018-03-19 15:17:03 +1100

    Copy existing config from eslint-config-tc

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "import/extensions": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/no-unresolved": "off",
+    "max-len": ["error", { "code": 120 }],
+    "no-undef": "off",
+    "quotes": "off",
+  }
+}
