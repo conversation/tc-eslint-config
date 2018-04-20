93f26a5d9cdfbdd43e2c712b63e149f17aafb8d9
Tiago Amaro <tiagopadrela@gmail.com>
2018-04-20 10:42:41 -0300

    Ignore JSX extra parenthesis for multi-line components
    Source: https://eslint.org/docs/rules/no-extra-parens#ignorejsx

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
     "import/no-extraneous-dependencies": "off",
     "import/no-unresolved": "off",
     "max-len": ["error", { "code": 120 }],
-    "no-extra-parens": ["error", "all", { "nestedBinaryExpressions": false }],
+    "no-extra-parens": ["error", "all", { "nestedBinaryExpressions": false, "ignoreJSX": "multi-line" }],
     "no-undef": "off",
     "quotes": ["error", "double"],
   }
