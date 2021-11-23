433f07a497904acbf58701edf41dccdd722582f9
Pawel Galazka <829242+pawelgalazka@users.noreply.github.com>
2021-11-24 10:49:21 +1100

    Update packages

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 // ref https://github.com/airbnb/javascript/blob/eslint-config-airbnb-v15.1.0/packages/eslint-config-airbnb-base/rules/variables.js
-const restrictedGlobals = require('eslint-restricted-globals');
+// const restrictedGlobals = require('eslint-restricted-globals');
 
 module.exports = {
-  "extends": "airbnb",
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "allowImportExportEverywhere": true
+  extends: "airbnb",
+  parser: "babel-eslint",
+  parserOptions: {
+    allowImportExportEverywhere: true,
   },
-  "rules": {
-    "comma-dangle": ["error", {
-      "arrays": "always-multiline",
-      "objects": "always-multiline",
-      "imports": "never",
-      "exports": "never",
-      "functions": "ignore"
-    }],
+  rules: {
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "never",
+        exports: "never",
+        functions: "ignore",
+      },
+    ],
     "function-paren-newline": 0,
     "import/extensions": "off",
     "import/no-extraneous-dependencies": "off",
     "import/no-unresolved": "off",
     "jsx-a11y/aria-role": ["warn", { ignoreNonDOM: true }],
-    "max-len": ["error", { "code": 120 }],
-    "no-extra-parens": ["error", "all", { "nestedBinaryExpressions": false, "ignoreJSX": "multi-line" }],
+    "max-len": ["error", { code: 120 }],
+    "no-extra-parens": [
+      "error",
+      "all",
+      { nestedBinaryExpressions: false, ignoreJSX: "multi-line" },
+    ],
     "no-undef": "off",
-    "no-restricted-globals": ["off"].concat(restrictedGlobals),
+    "no-restricted-globals": ["off"], //.concat(restrictedGlobals),
     "object-curly-newline": 0,
-    "quotes": ["error", "double"],
-  }
-}
+    quotes: ["error", "double"],
+  },
+};
